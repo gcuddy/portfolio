7804221e0df4ee761b7d7af8447f9c56fc907192
Gus <gus.cuddy@gmail.com>
2023-11-27 15:53:16 -0500

    ssr setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  output: "hybrid",
+  integrations: [tailwind(), svelte()],
+  adapter: netlify()
 });
